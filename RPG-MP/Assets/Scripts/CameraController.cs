1d18c5bffc81f57c75760e0b9001f2b384962291
Ronno777 <smithryan954@gmail.com>
2025-10-26 16:08:02 -0700

    New map, Updated camera controller to clamp to world, Misc collider fixes, Web build update

--- RPG-MP/Assets/Scripts/CameraController.cs
+++ RPG-MP/Assets/Scripts/CameraController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    private readonly Vector2 minBounds = new Vector2(-15f, -15f); // bottom-left world corner
+    private readonly Vector2 maxBounds = new Vector2(15f, 15f);   // top-right world corner
+
+    private Camera cam;
+    private float halfHeight;
+    private float halfWidth;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        halfHeight = cam.orthographicSize;
+        halfWidth = halfHeight * cam.aspect;
+    }
+
+    void LateUpdate()
     {
         // does the player exist?
         if (PlayerController.me != null && !PlayerController.me.dead)
         {
             Vector3 targetPos = PlayerController.me.transform.position;
             targetPos.z = -10;
-            transform.position = targetPos;
+
+            // clamp to map bounds
+            float clampedX = Mathf.Clamp(targetPos.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+            float clampedY = Mathf.Clamp(targetPos.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+
+            transform.position = new Vector3(clampedX, clampedY, targetPos.z);
         }
     }
 }
